fa86dc0db38ea56b9acc55a933317d681758386d
kimharim04 <ys9910706@gmail.com>
2024-12-09 10:50:06 +0900

    Update st_form.py

--- st_form.py
+++ st_form.py
@@ -5,7 +5,7 @@ st.title("21대 대통령선거")
 
 st.header('**투표 용지**')
 with st.form('my form'):
-        kr_president = st.selectbox('21대 대통령 후보', ['이재명', '한동훈', '조국', '이준석', '허경영', '홍길동', '강감찬'])
+        kr_president = st.selectbox('21대 대통령 후보', ['이재명', '한동훈', '조국', '이준석', '안철수', '허경영', '홍길동', '강감찬'])
         submitted = st.form_submit_button('제출')
 
 if submitted:
